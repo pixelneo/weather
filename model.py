9e8bb3947b5a48ab00f87bfaabf4a724da416477
Ondřej Měkota <ondra@Ondrejs-MacBook-Pro.local>
2019-08-02 15:24:09 +0200

    x

--- model.py
+++ model.py
@@ -32,7 +32,7 @@ def load_data(file, learn_window=48, predict_window=24, train=0.9, dev=0.05, tes
 
     return trainset, devset, testset, dim
 
-def lr_scheduler(initial=1e-4, final=1e-5):
+def lr_scheduler(initial=1.5e-4, final=2e-6):
     def s(epoch):
         return max(final, initial/((epoch+1)*2))
     return s
@@ -51,6 +51,7 @@ def create_model(predict_window=24, predict_feature=2, init_lr=0.0001, end_lr=0.
         tf.keras.layers.GRU(64, activation='relu', input_shape=[None, 9], return_sequences=True),
         tf.keras.layers.GRU(64, activation='relu'),
         tf.keras.layers.RepeatVector(predict_window),
+        tf.keras.layers.GRU(64, activation='relu', return_sequences=True),
         tf.keras.layers.GRU(64, activation='relu', return_sequences=True),
         tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(64, activation='relu')),
         tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(32)),
@@ -72,7 +73,7 @@ if __name__ == '__main__':
 
     model = create_model(dim=dim)
     callbacks = create_callbacks()
-    model.fit(train, epochs=8, validation_data=dev, workers=8, use_multiprocessing=True, callbacks=callbacks)
+    model.fit(train, epochs=10, validation_data=dev, workers=8, use_multiprocessing=True, callbacks=callbacks)
 
     test2 = test.take(1)
     preds = model.predict(test2)
